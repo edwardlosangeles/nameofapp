b5d2e88852e3fb9041fb6e954dec61d139f7e3dd
edwardlosangeles <edwardlosangeles@gmail.com>
2017-08-29 12:52:01 -0700

    4.2 test rename back to color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829173926) do
+ActiveRecord::Schema.define(version: 20170829194801) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170829173926) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "colour"
+    t.string "color"
   end
 
 end
