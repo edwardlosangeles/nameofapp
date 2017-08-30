a2577f4315100c55056af69d78155425b2b44146
edwardlosangeles <edwardlosangeles@gmail.com>
2017-08-30 11:37:55 -0700

    4.2 changed index layout and added images and price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829203241) do
+ActiveRecord::Schema.define(version: 20170830171359) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170829203241) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
+    t.decimal "price", precision: 8, scale: 2
   end
 
 end
