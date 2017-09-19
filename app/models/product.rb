d52996672d1064cff2435c6a8cdccea6242da8a8
edwardlosangeles <edwardlosangeles@gmail.com>
2017-09-19 02:40:59 -0700

    5.2 use ilike instead of LIKE if in prod env

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,23 @@ class Product < ApplicationRecord
 	has_many :orders
 
 
+	# ED 20170918 search form
+	def self.search(search_term)
+		
+		#In Postgres/Heroku LIKE is case sensitive, so use ilike
+		#use Rails.env.development? to check if you're in local
+		#use Rails.env.production? to check if you're in heroku
+		if Rails.env.development?
+			puts "we in the dev"#.where("name LIKE ?", "%#{search_term}%")
+			Product.where("name LIKE ?", "%#{search_term}%")
+		else
+			puts "we in the prod"
+			Product.where("name ilike ?", "%#{search_term}%")
+		end
+	end
 
-# ED 20170918 search form
-def self.search(search_term)
-	Product.where("name LIKE ?", "%#{search_term}%")
-end
+
+
 
 
 
