7b811f66e1f08d01677b317f27dc69cef0ab45ab
edwardlosangeles <edwardlosangeles@gmail.com>
2017-11-01 16:34:48 -0700

    5.10 ability modified for testing

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,14 @@ class Ability
     def initialize(user)
         user ||= User.new # guest user (not logged in)
         can :manage, User, id: user.id
+
+        if user.admin?
+        	can :destroy, Comment
+        else
+        	cannot :destroy, Comment
+        end
+
+        
     end
 
 end
