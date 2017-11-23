f400b7be052fc1fc7b322f1d42a98d291d77a35d
edwardlosangeles <edwardlosangeles@gmail.com>
2017-11-23 04:51:48 -0800

    6.4 factories and generating test data

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
 	
-	sequence(:email) { |n| "testing#{n}@example.com" }
+	sequence(:email) { |n| "testuser#{n}@example.com" }
 
 	factory :user, class: User do
 		email
@@ -10,6 +10,14 @@ FactoryBot.define do
 		first_name "peter1"
 		last_name "example"
 		admin false
+	end
+
+	factory :admin, class: User do
+		email
+		password "1234567890"
+		first_name "admin"
+		last_name "user"
+		admin true
 	end
 
 
