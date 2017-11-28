733bc1afe2b689ad90c67f3e425cafef91a29e9d
edwardlosangeles <edwardlosangeles@gmail.com>
2017-11-27 20:43:40 -0800

    6.5 Stripe remove product price column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126034353) do
+ActiveRecord::Schema.define(version: 20171128040118) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20171126034353) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
-    t.decimal "price", precision: 8, scale: 2
     t.integer "price_in_cents"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,12 @@
 #Product.create(name: "Prod3", description: "created in seeds.rb", image_url: "work/zion.jpg")
 
 
+Comment.create(user_id: 1, body: "it was just okay", rating: 3, product_id: 12)
+Comment.create(user_id: 1, body: "horrible", rating: 1, product_id: 12)
+Comment.create(user_id: 1, body: "Life changing", rating: 5, product_id: 12)
+Comment.create(user_id: 1, body: "just to increase the average", rating: 5, product_id: 12)
+Comment.create(user_id: 2, body: "test", rating: 5, product_id: 14)
+Comment.create(user_id: 2, body: "test", rating: 5, product_id: 13)
+Comment.create(user_id: 2, body: "test", rating: 4, product_id: 13)
+Comment.create(user_id: 2, body: "test", rating: 5, product_id: 13)
+Comment.create(user_id: 1, body: "test", rating: 2, product_id: 14)
