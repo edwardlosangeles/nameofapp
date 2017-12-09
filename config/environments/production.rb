cf4cdff57a11c201bdb69109aec3ad2bafa80a4c
edwardlosangeles <edwardlosangeles@gmail.com>
2017-12-08 22:27:07 -0800

    6.7 Rails.logger.level = 2 test 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.log_level = :warn
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-Rails.logger.level = 2
