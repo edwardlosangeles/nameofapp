a2de7dd5b394ffafeceff09cb9e3be2e04f1d355
edwardlosangeles <edwardlosangeles@gmail.com>
2017-12-21 14:18:49 -0800

    6.11 caching products index pagination error

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
 module ProductsHelper
+
+	# 20171220 6.11: Caching
+	def cache_key_for_products
+		count = Product.count
+		max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
+		"products/all-#{count}-#{max_updated_at}"
+	end
+
+	
 end
