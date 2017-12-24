b00ba65a3558ad4f7f65644ecca2102eb4dc735e
edwardlosangeles <edwardlosangeles@gmail.com>
2017-12-24 15:06:29 -0800

    6.12 test redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,6 @@
 # 20171222 6.12: Redis & Performance Strategy
 if Rails.env.production?
-	# $redis = Redis.new(url: ENV["REDIS_URL"])
-	puts "we in the prod"
+	$redis = Redis.new(url: ENV["REDIS_URL"])
 else
-	# $redis = Redis.new(host: 'localhost', port: 6379)
-	puts "we in the dev"
+	$redis = Redis.new(host: 'localhost', port: 6379)
 end
